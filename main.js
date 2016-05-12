fb2fa179879e868dd299b908a358c4fd6e1d079b
Moon Ahmed <planetbarsent@gmail.com>
2016-05-11 22:30:25 -0500

    JS functions

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
-function get_artist(artist_id) {
+var artist_id = '4dpARuHxo51G3z768sgnrY';
+var response_data;
+
+function get_artist() {
   var request = new XMLHttpRequest();
-  request.open("GET", "https://api.spotify.com/v1/artists/"+id, true);
+  request.open("GET", "https://api.spotify.com/v1/artists/"+artist_id, true);
   request.send();
   request.onreadystatechange = function () {
     //if request is not started completed
     if(request.readyState !=4 || request.status != 200) return;
-    console.log(request.response);
+    response_data = JSON.parse(request.response);
+    console.log(response_data);
   };
+};
+
+function clear_data() {
+  document.getElementById('').innerHTML = '';
 }
+
+document.getElementById("artist-submit").addEventListener("click", function(e) {
+	//artist_id = document.getElementById('artist').value;
+  console.log(artist_id);
+  get_artist();
+  e.preventDefault();
+});
 
 function get_related_artists(artist_id) {
   var request = new XMLHttpRequest();
@@ -24,5 +39,6 @@ function get_artist_top_tracks(artist_id) {
 }
 
 function get_preview_of_track(preview_url, artist_id) {
-  var track = get_artist_top_tracks(id)
+  var track = get_artist_top_tracks(id);
+
 }
