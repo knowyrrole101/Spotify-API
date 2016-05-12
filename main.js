93d179916b40ba88c87e1bdf043b48a825938b01
Moon Ahmed <planetbarsent@gmail.com>
2016-05-12 15:53:17 -0500

    Additional DOM and HTML changes, and created functions for specific API data pulls

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-var artist_id = '4dpARuHxo51G3z768sgnrY';
-var response_data,
-    artist_heading = document.getElementById('artist-heading'),
+var artist_id = '4dpARuHxo51G3z768sgnrY',
     artist_submit = document.getElementById('artist-submit'),
+    artist_heading = document.getElementById('artist-heading'),
     artist_link = document.getElementById('artist-link'),
     artist_image = document.getElementById('artist-image'),
     artist_followers = document.getElementById('artist-followers'),
@@ -9,15 +8,42 @@ var response_data,
 
 function get_artist() {
   var request = new XMLHttpRequest();
-  request.open("GET", "https://api.spotify.com/v1/artists/"+artist_id, true);
+  request.open("GET", "https://api.spotify.com/v1/artists/" + artist_id, true);
   request.send();
   request.onreadystatechange = function () {
     //if request is not started or completed
     if(request.readyState !=4 || request.status != 200) return;
-    response_data = JSON.parse(request.response);
+    var response_data = JSON.parse(request.response);
+    //DOM CHANGES HERE
     console.log(response_data);
   };
 };
+
+function get_related_artists() {
+  var request = new XMLHttpRequest();
+  request.open("GET", "https://api.spotify.com/v1/artists/"+ artist_id + "/related-artists", true);
+  request.send();
+  request.onreadystatechange = function() {
+    if(request.readyState !=4 || request.status != 200) return;
+    var related_artists_data = JSON.parse(request.response);
+    console.log(related_artists_data);
+  }
+}
+
+function get_artist_top_ten() {
+  var request = new XMLHttpRequest();
+  request.open("GET","https://api.spotify.com/v1/artists/"+ artist_id + "/top-tracks?country=US", true)
+  request.send();
+  request.onreadystatechange = function() {
+    if(request.readyState !=4 || request.status != 200) return;
+    var artist_top_tracks = JSON.parse(request.response);
+    console.log(artist_top_tracks);
+  }
+}
+
+function get_preview_of_track(preview_url, artist_id) {
+  var track = get_artist_top_tracks(id);
+}
 
 function clear_data() {
   //event listener on clear click set new variable values
@@ -32,26 +58,6 @@ function clear_data() {
 
 artist_submit.addEventListener("click", function(e) {
 	//artist_id = document.getElementById('artist').value;
-  console.log(artist_id);
   get_artist();
   e.preventDefault();
 });
-
-function get_related_artists(artist_id) {
-  var request = new XMLHttpRequest();
-  request.open("GET", "https://api.spotify.com/v1/artists/"+id+"/related-artists");
-  request.send();
-  request.onreadystatechange = function() {
-    if(request.readyState !=4 || request.status != 200) return;
-    console.log(request.response);
-  }
-}
-
-function get_artist_top_tracks(artist_id) {
-
-}
-
-function get_preview_of_track(preview_url, artist_id) {
-  var track = get_artist_top_tracks(id);
-
-}
